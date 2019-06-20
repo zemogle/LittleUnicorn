63cdb1c7daff01118f10f07e318095b452b132f3
Edward Gomez <edward@gomez.me.uk>
2019-06-20 14:01:53 +0100

    Remove print

--- client.py
+++ client.py
@@ -58,7 +58,6 @@ def wait_for_internet_connection():
 def display(sound):
     levels = np.abs((sound['data']*y)/2) #sound['data']
     rgb = []
-    print(np.mean(levels))
     if np.mean(levels) < 0.03:
         cry = True
     else:
