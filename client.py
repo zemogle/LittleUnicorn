02141d380697068448d3ca342a4d016be477e873
Edward Gomez <edward@gomez.me.uk>
2019-06-28 22:17:02 +0100

    Use distinct levels for sound

--- client.py
+++ client.py
@@ -10,7 +10,11 @@ from urllib.error import URLError
 from urllib.request import urlopen
 import time
 from random import randint
+from datetime import datetime
 import numpy as np
+import requests
+import config
+
 
 try:
     import unicornhathd
@@ -26,7 +30,7 @@ wifi_rgb = [
     [255,99,71], [255,215,0], [50,205,50], [0,255,255], [30,144,255], [220,20,60]
 ]
 
-pixels   =   [
+PIXELS   =   [
         [0,0,1,0,0,0,0,0,1,0,0],
         [0,0,0,1,0,0,0,1,0,0,0],
         [0,0,1,1,1,1,1,1,1,0,0],
@@ -55,11 +59,12 @@ def wait_for_internet_connection():
             awaiting_connection()
     return
 
-def display(sound):
+def display(sound, lastcry):
     levels = np.abs((sound['data']*y)) #sound['data']
     rgb = []
     if np.mean(levels) > 0.05:
         cry = True
+        # cry_alert(lastcry)
     else:
         cry = False
     for i in range(1,256):
@@ -79,20 +84,30 @@ def colourise(val, cry):
     return colour
 
 def val_to_hsv(val, cry):
-    if val > 1:
+    if cry:
+        hsv = (0.5,0.7,0.7)
+    elif val > 1:
         val = 1
         hsv = (1,1,0.5)
     elif val < 0:
         val = 0
         hsv = (0,0,0)
+    elif val < 0.6:
+        hsv = (0,0,0)
+    elif val >= 0.6 and val <= 0.7:
+        hsv = (0.13, 0.7, 0.7)
+    elif val > 0.7 and val <= 0.8:
+        hsv = (0.07, 0.7, 0.7)
+    elif val > 0.8 and val < 1:
+        hsv = (0.07, 0, 1)
     else:
-        hsv = (val, 1, val/3)
-    if cry:
-        hsv = (val,val ,0.6)
+        hsv = (val, 0.7, 1)
+
     return hsv
 
 
 async def main():
+    lastcry = datetime.now()
     session = aiohttp.ClientSession()
     async with session.ws_connect(URL) as ws:
         while True:
@@ -102,7 +117,7 @@ async def main():
                 # await prompt_and_send(ws)
                 await ws.send_str('')
                 sound = json.loads(msg.data)
-                colours = display(sound)
+                colours = display(sound, lastcry)
                 for i, colour in enumerate(colours):
                     x,y = divmod(i,16)
                     unicornhathd.set_pixel_hsv(x, y, colour[0], colour[1], colour[2])
@@ -119,15 +134,26 @@ async def main():
 def awaiting_connection():
     x = 7
     rgb = wifi_rgb[randint(0,5)]
-    for row in range(0,len(pixels)):
-        for col in range(0,len(pixels[row])):
-            pixel=pixels[row][col]
+    for row in range(0,len(PIXELS)):
+        for col in range(0,len(PIXELS[row])):
+            pixel=PIXELS[row][col]
             if pixel>0:
                 unicornhathd.set_pixel(col, row, rgb[0], rgb[1], rgb[2])
             else:
                 unicornhathd.set_pixel(col, row, 0,0,0)
     unicornhathd.show()
     time.sleep(0.1)
+    return
+
+def cry_alert(lastcry):
+    if (datetime.now() - lastcry).seconds > 300:
+        r = requests.post("https://api.pushover.net/1/messages.json", data = {
+          "token": config.TOKEN,
+          "user": config.USER_KEY,
+          "message": "Crying baby!"
+        })
+        LASTCRY = datetime.now()
+
     return
 
 if __name__ == '__main__':
